4fee40f57402f96e5e3e0101650db38512fbb2c6
Ishwarya <128075672+Ishwaryathiru@users.noreply.github.com>
2024-02-13 14:47:30 +0530

    sms.py

--- sms.py
+++ sms.py
@@ -1,5 +1,5 @@
 from twilio.rest import Client
-SID='ACaf51e5854a5c526af13c5d3104888fc2'
-token='0c469fe8ae188cbe02577c9a14cbd899'
+SID='ACaf51e5854a5c5'
+token='0c469fe8ae188cbe025'
 ct=Client(SID,token)
-ct.messages.create(body="hi this message is sent using twilio",from_='+13344543523',to='+918870248416')
+ct.messages.create(body="hi this message is sent using twilio",from_='+133',to='+91##########')
